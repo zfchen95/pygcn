24b1e7d51d5bc65142a5f94cb5d789468d1b55bd
Zhenfeng Chen <zhchen@nvidia.com>
2018-07-17 08:08:26 -0700

    merge with origin

---
+++ pygcn/train.py
@@ -0,0 +1,134 @@
+from __future__ import division
+from __future__ import print_function
+
+import time
+import argparse
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+from pygcn.utils import load_data, accuracy
+from pygcn.models import GCN
+
+# Training settings
+parser = argparse.ArgumentParser()
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true', default=False,
+                    help='Validate during training pass.')
+parser.add_argument('--seed', type=int, default=42, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=200,
+                    help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=0.01,
+                    help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=5e-4,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=16,
+                    help='Number of hidden units.')
+parser.add_argument('--dropout', type=float, default=0.5,
+                    help='Dropout rate (1 - keep probability).')
+parser.add_argument('--train', type=str, default='ip',
+                    help='Training dataset.')
+parser.add_argument('--test', type=str, default='ip',
+                    help='Test dataset, used for prediction.')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+train_dataset = args.train
+test_dataset = args.test
+
+# Load data
+adj, features, labels, idx_train, idx_val, idx_test = load_data(dataset=train_dataset)
+# Model and optimizer
+model = GCN(nfeat=features.shape[1],
+            nhid=args.hidden,
+            nclass=labels.max() + 1,
+            dropout=args.dropout)
+optimizer = optim.Adam(model.parameters(),
+                       lr=args.lr, weight_decay=args.weight_decay)
+
+if args.cuda:
+    model.cuda()
+    features = features.cuda()
+    adj = adj.cuda()
+    labels = labels.cuda()
+    idx_train = idx_train.cuda()
+    idx_val = idx_val.cuda()
+    idx_test = idx_test.cuda()
+
+features, labels = Variable(features), Variable(labels)
+
+
+def train(epoch):
+    t = time.time()
+    model.train()
+    optimizer.zero_grad()
+    output = model(features, adj)
+    loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+    acc_train = accuracy(output[idx_train], labels[idx_train])
+    loss_train.backward()
+    optimizer.step()
+
+    if not args.fastmode:
+        # Evaluate validation set performance separately,
+        # deactivates dropout during validation run.
+        model.eval()
+        output = model(features, adj)
+
+    loss_val = F.nll_loss(output[idx_val], labels[idx_val])
+    acc_val = accuracy(output[idx_val], labels[idx_val])
+    print('Epoch: {:04d}'.format(epoch+1),
+          'loss_train: {:.4f}'.format(loss_train.data[0]),
+          'acc_train: {:.4f}'.format(acc_train.data[0]),
+          'loss_val: {:.4f}'.format(loss_val.data[0]),
+          'acc_val: {:.4f}'.format(acc_val.data[0]),
+          'time: {:.4f}s'.format(time.time() - t))
+
+
+def test():
+    model.eval()
+    output = model(features, adj)
+    loss_test = F.nll_loss(output[idx_test], labels[idx_test])
+    acc_test = accuracy(output[idx_test], labels[idx_test])
+    print("Test set results:",
+          "loss= {:.4f}".format(loss_test.data[0]),
+          "accuracy= {:.4f}".format(acc_test.data[0]))
+
+def predict():
+    model.eval()
+    adj, features, _, _, _, _ = load_data(dataset=test_dataset)
+    if args.cuda:
+        features = features.cuda()
+        adj = adj.cuda()
+    features = Variable(features)
+    outputs = model(features, adj)
+    predicted = outputs.data
+    # _, predicted = torch.max(outputs.data, 0)
+    predictions = (list(predicted.cpu().numpy()))
+    with open("%s_prediction.txt" % test_dataset, 'wb') as f:
+        for index, label in enumerate(labels.data.cpu().numpy()):
+            f.write('%d %d\n' % (index, label))
+            # print(index, label)
+    print("Prediction completed!")
+
+# Train model
+t_total = time.time()
+for epoch in range(args.epochs):
+    train(epoch)
+print("Optimization Finished!")
+print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+
+# Testing
+test()
+
+# Prediction
+# predict()
